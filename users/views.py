795caab0ae7075694d4c7744846e14e14e873a95
tarricsookdeo <tarricsookdeo@outlook.com>
2020-10-20 03:46:57 -0400

    Add permission class to UserListView

--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
 from rest_framework.generics import ListAPIView
+from rest_framework.permissions import IsAdminUser
 
 from .models import CustomUser
 from .serializers import UserSerializer
@@ -7,3 +8,4 @@ from .serializers import UserSerializer
 class UserListView(ListAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = UserSerializer
+    permission_classes = [IsAdminUser]
