a70b17ce61169b0956ca59fe960fef57debef277
tarricsookdeo <tarricsookdeo@outlook.com>
2020-10-20 18:11:08 -0400

    Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import Navbar from './components/layout/Navbar';
 
 const App = () => {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <Navbar />
+    </div>
+  );
 };
 
 export default App;
---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav className='navbar navbar-expand-lg navbar-light bg-light'>
+      <div className='container'>
+        <a className='navbar-brand' href='#'>
+          Navbar
+        </a>
+        <button
+          className='navbar-toggler'
+          type='button'
+          data-toggle='collapse'
+          data-target='#navbarSupportedContent'
+          aria-controls='navbarSupportedContent'
+          aria-expanded='false'
+          aria-label='Toggle navigation'
+        >
+          <span className='navbar-toggler-icon'></span>
+        </button>
+
+        <div className='collapse navbar-collapse' id='navbarSupportedContent'>
+          <ul className='navbar-nav ml-auto'>
+            <li className='nav-item'>
+              <a className='nav-link' href='#'>
+                Login
+              </a>
+            </li>
+            <li className='nav-item'>
+              <a className='nav-link' href='#'>
+                Signup
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
