8a8b2337688a321f9ab0af78a1b2bd5663782da6
tarricsookdeo <tarricsookdeo@outlook.com>
2020-10-23 05:04:30 -0400

    Intial login request

--- client/src/actions/userActions.js
+++ client/src/actions/userActions.js
@@ -1,24 +1,27 @@
-// import { LOGIN_USER, REGISTER_USER } from './types';
+import { LOGIN_USER } from './types';
 
 // Login user
-export const loginUser = (email, password) => {
+export const loginUser = (email, password) => async dispatch => {
   const data = {
     email: email,
     password: password
   };
 
-  fetch('http://127.0.0.1:8000/api/v1/users/auth/login/', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(data)
-  })
-    .then(response => response.json())
-    .then(data => {
-      console.log(data);
-    })
-    .catch(err => {
-      console.log(err);
-    });
+  const request = await fetch(
+    'http://127.0.0.1:8000/api/v1/users/auth/login/',
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    }
+  );
+
+  const response = await request.json();
+
+  dispatch({
+    type: LOGIN_USER,
+    payload: response.key
+  });
 };
