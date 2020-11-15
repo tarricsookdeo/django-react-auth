9d729ff3145c96fa2fa3ca649b8f5402bbc91a38
tarricsookdeo <tarricsookdeo@outlook.com>
2020-11-15 02:29:46 -0500

    Delete react frontend client

--- client/src/actions/types.js
+++
@@ -1,2 +0,0 @@
-export const REGISTER_USER = 'REGISTER_USER';
-export const LOGIN_USER = 'LOGIN_USER';
--- client/src/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux';
-import userReducer from './userReducer';
-
-export default combineReducers({
-  user: userReducer
-});
