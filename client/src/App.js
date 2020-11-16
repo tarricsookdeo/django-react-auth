c3e5b19f484db9a9b4818cf67fdf095daaa2c117
tarricsookdeo <tarricsookdeo@outlook.com>
2020-11-15 20:41:19 -0500

    Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,15 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+
 const App = () => {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <Router>
+        <Navbar />
+        <Switch></Switch>
+      </Router>
+    </div>
+  );
 };
 
 export default App;
---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  const [isAuth, setIsAuth] = useState(false);
+
+  useEffect(() => {
+    // check for auth token in local storage
+    // if there is a token, setIsAuth to true
+  }, [isAuth]);
+
+  if (isAuth) {
+    return (
+      <nav>
+        <h1>Django React Auth</h1>
+        <ul>
+          <li>
+            <Link to='/logout'>Logout</Link>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
+  return (
+    <nav>
+      <h1>Django React Auth</h1>
+      <ul>
+        <li>
+          <Link to='/login'>Login</Link>
+        </li>
+        <li>
+          <Link to='/signup'>Signup</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
