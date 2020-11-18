bb47688e69f530d24437fcf194d618b8a1598fc2
tarricsookdeo <tarricsookdeo@outlook.com>
2020-11-18 02:05:28 -0500

    Add email to dashboard

--- client/src/views/app/Dashboard.js
+++ client/src/views/app/Dashboard.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 
 const Dashboard = () => {
+  const [userEmail, setUserEmail] = useState();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (localStorage.getItem('token') == null) {
+      window.location.replace('http://localhost:3000/login');
+    } else {
+      fetch('http://127.0.0.1:8000/api/v1/users/auth/user/', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Token ${localStorage.getItem('token')}`
+        }
+      })
+        .then(res => res.json())
+        .then(data => {
+          setUserEmail(data.email);
+          setLoading(false);
+        });
+    }
+  }, []);
+
   return (
     <div>
-      <h1>Dashboard</h1>
+      {loading === false && (
+        <Fragment>
+          <h1>Dashboard</h1>
+          <h2>Hello {userEmail}!</h2>
+        </Fragment>
+      )}
     </div>
   );
 };
