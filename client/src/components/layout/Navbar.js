be474a6577d33e6e8f024b9b64a841959c09f428
tarricsookdeo <tarricsookdeo@outlook.com>
2020-11-22 06:21:52 -0500

    Added ternary operator to Navbar render

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,42 +1,40 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   const [isAuth, setIsAuth] = useState(false);
 
   useEffect(() => {
-    // check for auth token in local storage
-    // if there is a token, setIsAuth to true
     if (localStorage.getItem('token') !== null) {
       setIsAuth(true);
     }
   }, [isAuth]);
 
-  if (isAuth) {
-    return (
-      <nav>
-        <h1>Django React Auth</h1>
-        <ul>
-          <li>
-            <Link to='/dashboard'>Dashboard</Link>
-          </li>
-          <li>
-            <Link to='/logout'>Logout</Link>
-          </li>
-        </ul>
-      </nav>
-    );
-  }
   return (
     <nav>
       <h1>Django React Auth</h1>
       <ul>
-        <li>
-          <Link to='/login'>Login</Link>
-        </li>
-        <li>
-          <Link to='/signup'>Signup</Link>
-        </li>
+        {isAuth === true ? (
+          <Fragment>
+            {' '}
+            <li>
+              <Link to='/dashboard'>Dashboard</Link>
+            </li>
+            <li>
+              <Link to='/logout'>Logout</Link>
+            </li>
+          </Fragment>
+        ) : (
+          <Fragment>
+            {' '}
+            <li>
+              <Link to='/login'>Login</Link>
+            </li>
+            <li>
+              <Link to='/signup'>Signup</Link>
+            </li>
+          </Fragment>
+        )}
       </ul>
     </nav>
   );
